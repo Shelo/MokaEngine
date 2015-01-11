4e1670cd4bf5b98fab3cf5ae6f1d09746836acf9
Shelo <marjara35@gmail.com>
2015-01-11 10:35:12 -0400

    Create src dir.

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
 #include "Core.h"
-=======
-#include <iostream>
-#include "Core.h"
-#include "BaseGame.h"
-#include "Mesh.h"
-#include <vector>
->>>>>>> 3ef377a769b287de0d5c00b006d08d7e4aefc667
 #include "Texture.h"
 #include "Triangler.h"
 
@@ -28,11 +20,7 @@ public:
 	Texture *texture;
 
 	void Create() {
-<<<<<<< HEAD
 		texture = new Texture("res/textures/bricks2.jpg");
-=======
-		texture = new Texture("./res/textures/bricks2.jpg");
->>>>>>> 3ef377a769b287de0d5c00b006d08d7e4aefc667
 		texture->Bind();
 
 		GameObject *p = new GameObject(0.0f, 0.0f);
@@ -50,10 +38,5 @@ int main(int argc, char** argv) {
 	core::Core core(640, 480, 60, new Game());
 	core.CreateDisplay("Moka Engine");
 	core.Start();
-<<<<<<< HEAD
-=======
-
-	system("pause");
->>>>>>> 3ef377a769b287de0d5c00b006d08d7e4aefc667
 	return 0;
 }
